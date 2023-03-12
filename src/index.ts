5329aff516426ae24d106618f77b786aca9bf3ac
Chris Papazian <chris.papazian@gmail.com>
2023-03-11 18:21:24 -0800

    working first pass
    - renders spreads to a portrait
    - detects pixel size from first image
    - no margin support

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,30 @@ type Filename = string;
 type Dimensions = {
   height: number,
   width: number,
+  unit?: 'inches' | 'in' | 'cm',
 };
 
 type Pane = 'right' | 'left';
+
+type Config = {
+  images: Filename[],
+  realDimensions: Dimensions,
+  paperSize: {
+    dimensions: Dimensions,
+    margin?: number
+  },
+  format?: 'spread' | 'page',
+  backIsFirst?: boolean,
+};
+
+type Layout = {
+  paperSize: Dimensions,
+  gridLayout: {
+    x: number,
+    y: number,
+  },
+  padding: number,
+};
 
 program
   .name('zf')
@@ -33,16 +54,20 @@ getConfig(opts).then(async config => {
   // 2. figure out image dimensions in pixels
   const dimensions = await getDimensions(config.images);
   // 3. process input images
-  const pages = await processInputImages(config.images, dimensions, tempDir);
+  const pages = await processInputImages(config, dimensions, tempDir);
   console.log(pages);
 
   // 4. create new "spreads"
   const spreads = await createSpreadsFromPages(pages, tempDir)
   console.log(spreads);
 
-// 5. calculate most efficient layout
+  // 5. calculate most efficient layout
+  const layout = await calculateLayout(config);
+  console.log(layout);
 
-// 6. render spreads onto canvas
+  // 6. render spreads onto canvas
+  const bigPages = await renderSpreads(layout, spreads);
+  // console.log(bigPages);
 
 // 7. compile to pdf/tiff
 
@@ -50,7 +75,7 @@ getConfig(opts).then(async config => {
 });
 
 
-async function getConfig(opts: OptsType) {
+async function getConfig(opts) {
   if (opts.config) {
     const file = await fs.readFile(opts.config, 'utf-8');
     return JSON.parse(file);
@@ -59,8 +84,16 @@ async function getConfig(opts: OptsType) {
 
 async function getDimensions(images: Filename[]) {
   // TODO: examine files and determine dimensions programatically
-  const height = 800;
-  const width = 800;
+  // const height = 800;
+  // const width = 800;
+  const response = await magick({
+    args: ['identify', '-format', '%wx%h', images[0] ],
+    captureStdout: true
+  });
+  const [ height, width ] = response.split('x');
+  // console.log(response);
+  // const height = 3300;
+  // const width = 2550; //5100x3300
   return { height, width };
 }
 
@@ -69,19 +102,20 @@ async function processImage(
   dimensions: Dimensions,
   pane: Pane,
   outFile: Filename,
-): Promise<boolean> {
+) {
   const { height, width } = dimensions;
   const cropOffset = pane === 'left' ? 0 : width;
   const cropArgs = `${width}x${height}+${cropOffset}+0`;
   const args = [ imagePath, '-crop', cropArgs, outFile ];
-  return magick({ args });
+  magick({ args });
 }
 
 async function processInputImages(
-  images: Filename[],
+  config: Config,
   dimensions: Dimensions,
   tempDir: Filename
 ) {
+  const images = config.backIsFirst ? [...config.images, config.images[0]] : config.images;
   const pages: Filename[] = [];
   // await images.forEach(async (image, idx) => {
   for (let i = 0; i < images.length; i++) {
@@ -90,10 +124,8 @@ async function processInputImages(
     // in which case, it is processes as the final image)
     if (i !== 0) {
       const leftFileName = `${tempDir}/page-${i * 2 - 1}.png`;
-      if (!await processImage(image, dimensions, "left", leftFileName)) {
-        // TODO: handle errors
-      }
-      console.log(leftFileName);
+      await processImage(image, dimensions, "left", leftFileName);
+      // TODO: handle errors
       pages.push(leftFileName);
     }
 
@@ -101,11 +133,8 @@ async function processInputImages(
     // option is specified, in which case the last page will have already been handled above)
     if (i !== images.length - 1) {
       const rightFileName = `${tempDir}/page-${i * 2}.png`;
-      if (!await processImage(image, dimensions, "right", rightFileName)) {
-        // TODO: handle errors
-      }
+      await processImage(image, dimensions, "right", rightFileName);
       // TODO: handle errors
-      console.log(rightFileName);
       pages.push(rightFileName);
     }
   }
@@ -117,9 +146,14 @@ async function createSpread(
   right: Filename,
   outFile: Filename
 ) {
-  // magick montage -mode concatenate -tile x1  out2.png out.png montage.png
   const cmd = 'magick';
-  const args = ['montage', '-mode', 'concatenate', '-tile', 'x1', left, right, outFile];
+  const args = [
+    'montage',
+    '-mode', 'concatenate',
+    '-tile', 'x1',
+    left, right,
+    outFile
+  ];
   return magick({ args });
 }
 
@@ -144,4 +178,155 @@ async function createSpreadsFromPages(
     }
   }
   return spreads;
+}
+
+function _calculateLayout(
+  horizontalSpace: number, 
+  verticalSpace: number,
+  spreadWidth: number,
+  spreadHeight: number
+) {
+  console.log({
+    horizontalSpace,
+    verticalSpace,
+    spreadWidth,
+    spreadHeight,
+  })
+  const x = Math.trunc(horizontalSpace / spreadWidth);
+  const y = Math.trunc(verticalSpace / spreadHeight);
+  return { x, y };
+}
+
+function calculateLayout(config: Config) {
+  const marginX = 2 * (config.paperSize?.margin || 0);
+  const marginY = 2 * (config.paperSize?.margin || 0);
+  
+  const horizontalSpace = (config.paperSize?.dimensions?.width || 8.5) - marginX;
+  const verticalSpace = (config.paperSize?.dimensions?.height || 8.5) - marginY;
+
+  const spreadWidth = 2 * (config.realDimensions.width);
+  const spreadHeight = config.realDimensions.height;
+
+  // portrait
+  const portraitLayout = _calculateLayout(
+    horizontalSpace,
+    verticalSpace,
+    spreadWidth,
+    spreadHeight
+  );
+
+  return { 
+    paperSize: {
+      height: verticalSpace,
+      width: horizontalSpace,
+      // unit: 
+    },
+    gridLayout: portraitLayout,
+    padding: 0, // TODO: figure out padding
+  };
+
+  // // landscape
+  // const landscapeLayout = _calculateLayout(
+  //   verticalSpace,
+  //   horizontalSpace,
+  //   spreadWidth,
+  //   spreadHeight
+  // );
+
+  // if (
+  //   portraitLayout.x * portraitLayout.y >= 
+  //   landscapeLayout.x * landscapeLayout.y
+  // ) {
+  //   return { 
+  //     paperSize: {
+  //       height: verticalSpace,
+  //       width: horizontalSpace,
+  //       // unit: 
+  //     },
+  //     gridLayout: portraitLayout,
+  //     padding: 10, // TODO: figure out padding
+  //   };
+  // } else {
+  //   return { 
+  //     paperSize: {
+  //       height: horizontalSpace,
+  //       width: verticalSpace,
+  //       // unit: 
+  //     },
+  //     gridLayout: landscapeLayout,
+  //     padding: 10, // TODO: figure out padding
+  //   };
+  // }
+}
+
+
+async function renderOnePage(layout: Layout, spreads: Filename[], out: Filename) {
+  const { padding, gridLayout: { x, y } } = layout;
+  const tileArgs = `${x}x${y}`;
+  // const spacingArgs = `+${padding}+${padding}`;
+  const args = [
+    'montage',
+    // TODO: figure out how to preserve colors
+    // '-colorspace', 'sRGB',
+    '-mode', 'concatenate',
+    '-tile', tileArgs,
+    // '-geometry', spacingArgs,
+    // '-frame', '10x10',
+    // '-matteColor', 'none',
+    ...spreads,
+    out,
+  ];
+  return magick({ args });
+}
+
+async function renderSpreads(layout: Layout, spreads: Filename[]) {
+  const { gridLayout: { x, y } } = layout;
+  const capacity = x * y; 
+  const pages = [];
+  
+  let front = [];
+  let back = [];
+
+
+  for (let i = 0; i < spreads.length; i += 2) {
+    // adding images to the front is easy; they just go in order
+    // adding images to the back is harder, as we have to place right to left, top to bottom
+    const row = Math.trunc(front.length / x);
+    const column = front.length % x;
+    
+    front.push(spreads[i]);
+    const reverseIdx = (row * x) + (x - column - 1);
+    back[reverseIdx] = spreads[i+1]
+
+    if (front.length === capacity) {
+      const frontFilename = `${tempDir}/result-${pages.length}-front-${pages.length / 2}.png`;
+      await renderOnePage(layout, front, frontFilename);
+      pages.push(frontFilename);
+      front = [];
+
+      const backFilename = `${tempDir}/result-${pages.length}-back-${Math.trunc(pages.length / 2)}.png`;
+      await renderOnePage(layout, back, backFilename);
+      pages.push(backFilename);
+      back = []
+    }
+  }
+
+  if (front.length > 0) {
+    // create filler image for empty space in "back" pic
+    const filler = `${tempDir}/filler-spread.png`;
+    for (let i = 0; i < back.length; i++) {
+      if (!back[i]) {
+        back[i]  = filler;
+      }
+    }
+    
+    const frontFilename = `${tempDir}/result-${pages.length}-front-${pages.length / 2}.png`;
+    await renderOnePage(layout, front, frontFilename);
+    pages.push(frontFilename);
+    const backFilename = `${tempDir}/result-${pages.length}-back-${Math.trunc(pages.length / 2)}.png`;
+    await renderOnePage(layout, back, backFilename);
+    pages.push(backFilename);
+  }
+
+  return pages;
 }
--- src/magick.ts
+++ src/magick.ts
@@ -3,21 +3,39 @@ import { execFile } from "child_process";
 type ArgType = {
   args: string[],
   cmd?: string,
+  captureStdout?: boolean,
+  verbose?: boolean,
 };
 
 export default async function magick({
   args,
   cmd = 'magick',
-}: ArgType): Promise<boolean> {
+  captureStdout = false,
+  verbose = false,
+}: ArgType): Promise<string> {
   return new Promise((resolve, reject) => {
-    const process = execFile(cmd, args);
-    process.on('exit', (code) => {
+    const child = execFile(cmd, args);
+    
+    let stdout: string;
+    if (captureStdout && child.stdout) {
+      stdout = '';
+      child.stdout.on('data', (data) => {
+        stdout += data.toString();
+      });
+    }
+
+    if (verbose && child.stderr) {
+      child.stderr.on('data', (data) => {
+        console.error(data);
+      });
+    }
+        
+    child.on('exit', (code) => {
       if (code === 0) {
-        resolve(true);
+        resolve(stdout);
       } else {
-        // TODO: handle errors
-        // TODO: read stderr and reject with that string
-        reject('errp!')
+        // console.error(`exit code: ${code}`);
+        reject(`exit code: ${code}`);
       }
     });
   });
