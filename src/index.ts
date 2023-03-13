3d81a2dd42d71c8af65beb43c306f4cd99f77ee1
Chris Papazian <chris.papazian@gmail.com>
2023-03-12 21:32:29 -0700

    working version of layer/mosaic approach

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ type Dimensions = {
   unit?: 'inches' | 'in' | 'cm',
 };
 
+type Coordinates = { x: number, y: number };
+
 type Pane = 'right' | 'left';
 
 type Config = {
@@ -55,26 +57,20 @@ getConfig(opts).then(async config => {
   // 3. generate crop commands to extract individual pages from input images
   const pages = await processInputImages(config, tempDir);
   console.log(pages);
+  // NOTE: this output can also be used to generate thumbnails
 
   // 4. calculate most efficient layout
   const layout = await calculateLayout(config);
   console.log(layout);
 
+  // 5. determine final position on the page for each cropped page and express
+  //    as an imagemagick command
   const out = await calculatePositions(pages, layout);
   // console.log(out.map(x => x.flat().join(' ')));
 
+  // 6. execute the commands and render into pages
   const page = await mergeLayers(out);
   console.log(page)
-  // 4. create new "spreads"
-  // const spreads = await createSpreadsFromPages(pages, tempDir)
-  // console.log(spreads);
-
-  
-  // 6. render spreads onto canvas
-  // const bigPages = await renderSpreads(layout, spreads);
-  // console.log(bigPages);
-
-// 7. compile to pdf/tiff
 
 
 });
@@ -144,6 +140,17 @@ async function processInputImages(
 }
 
 
+function genPosition(base: string[], position: Coordinates, imageDimensions: Dimensions) {
+  const x = imageDimensions.width * (position.x + 1);
+  const y = imageDimensions.height * (position.y + 1);
+  return [
+    '(', ...base, ')',
+    '-background', 'none',
+    '-gravity', 'southeast',
+    '-extent', `${x}x${y}`,
+  ];
+}
+
 async function calculatePositions(pages: string[][], layout: Layout) {
   // while pages remain ... 
   //   assemble pages for front
@@ -165,8 +172,11 @@ async function calculatePositions(pages: string[][], layout: Layout) {
   // const pageGeometry = 
   // let pages = [];
 
-  const imageWidth = 800;
-  const imageHeight = 800;
+  // 5100x3300
+  // const imageWidth = 800;
+  // const imageHeight = 800;
+  
+  const imageDimeniosn = { width: 2550, height: 3300 };
 
   const numColumns = layout.gridLayout.x;
   const numRows = layout.gridLayout.y;
@@ -176,61 +186,43 @@ async function calculatePositions(pages: string[][], layout: Layout) {
 
   let front = [], back = [];
   for (let i=0, j=pages.length-1; i < j; i+=2, j-=2) {
-    console.log({ i, j });
-    // POP last page/command for front/left
-    const frontLeft = pages[j];
-    const frontLeftOffset = {
-      x: column * 2 * imageWidth,
-      y: row * imageHeight,
-    };
-    console.log(frontLeft, frontLeftOffset);
-    front.push([
-      '(', ...frontLeft, ')',
-      '-background', 'none',
-      '-gravity', 'southeast',
-      '-extent', `${frontLeftOffset.x + imageWidth}x${frontLeftOffset.y+imageHeight}`,
-    ]);
+    
+    // front left
+    front.push(
+      genPosition(
+        pages[j], 
+        { x: column * 2, y: row },
+        imageDimeniosn,
+      )
+    );
 
-    const frontRight = pages[i];
-    const frontRightOffset = {
-      x: ((column * 2) + 1) * imageWidth,
-      y: row * imageHeight,
-    };
-    console.log(frontRight, frontRightOffset);
-    front.push([
-      '(', ...frontRight, ')',
-      '-background', 'none',
-      '-gravity', 'southeast',
-      '-extent', `${frontRightOffset.x + imageWidth}x${frontRightOffset.y+imageHeight}`,
-    ]);
+    // front right
+    front.push(
+      genPosition(
+        pages[i], 
+        { x: (column * 2) + 1, y: row },
+        imageDimeniosn,
+      )
+    );
+    
+    // back right 
+    back.push(
+      genPosition(
+        pages[j-1], 
+        { x: (2 * (numColumns - column) - 1), y: row },
+        imageDimeniosn,
+      )
+    );
 
-    const backRight = pages[j-1];
-    const backRightOffset = {
-      x: imageWidth * (2 * (numColumns - column - 1)),
-      y: row * imageHeight,
-    }
-    console.log(backRight, backRightOffset);
-    back.push([
-      '(', ...backRight, ')',
-      '-background', 'none',
-      '-gravity', 'southeast',
-      '-extent', `${backRightOffset.x + imageWidth}x${backRightOffset.y+imageHeight}`,
-    ])
+    // back left 
+    back.push(
+      genPosition(
+        pages[i+1], 
+        { x: 2 * (numColumns - column - 1), y: row  },
+        imageDimeniosn,
+      )
+    );
 
-    const backLeft = pages[i+1];
-    const backLeftOffset = {
-      x: imageWidth * (2 * (numColumns - column - 1) - 1),
-      y: row * imageHeight,
-    }
-    console.log(backLeft, backLeftOffset);
-    back.push([
-      '(', ...backLeft, ')',
-      '-background', 'none',
-      '-gravity', 'southeast',
-      '-extent', `${backLeftOffset.x + imageWidth}x${backLeftOffset.y+imageHeight}`,
-    ])
-
-    console.log(column, row);
     column ++;
     if (column === numColumns) {
       column = 0;
@@ -250,60 +242,12 @@ async function mergeLayers(pages: string[][]) {
       ...(pages[i].map(page => ['(', ...page, ')'])).flat(),
       '-gravity', 'northwest',
       '-layers', 'mosaic',
-
-      // '(', ...pages[i][0], ')',
-      // '-gravity', 'northeast',
-      // '(', ...pages[i][1], ')',
-      // '-gravity', 'northeast',
-      // '-composite',
       `${tempDir}/page-${i}.png`,
     ];
 
     console.log(args);
     console.log(await magick({ args }));
-  }
-
-
-  
-}
-
-
-
-async function createSpread(
-  left: Filename,
-  right: Filename,
-  outFile: Filename
-) {
-  const args = [
-    left,
-    right,
-    '+append',
-    outFile
-  ].flat();
-  console.log(args, args.join(' '));
-  return magick({ args });
-}
-
-async function createSpreadsFromPages(
-  pages: Filename[],
-  tempDir: Filename,
-) {
-  const spreads: string[] = [];
-  for (let i = 0; i < pages.length / 2; i++) {
-    let left, right;
-    if (i % 2 === 0) {
-      left = pages[pages.length - 1 - i];
-      right = pages[i];
-    } else {
-      left = pages[i];
-      right = pages[pages.length - 1 - i];
-    }
-
-    const outFile = `${tempDir}/spread-${i}.png`;
-    await createSpread(left, right, outFile)
-    spreads.push(outFile);
-  }
-  return spreads;
+  }  
 }
 
 function _calculateLayout(
@@ -389,85 +333,3 @@ function calculateLayout(config: Config) {
   // }
 }
 
-
-async function renderOnePage(layout: Layout, spreads: Filename[], out: Filename) {
-  const { x, y } = layout.gridLayout;
-  const tileArgs = `${x}x${y}`;
-  const args = [
-    'montage',
-    '-mode', 'concatenate',
-    '-tile', tileArgs,
-    ...spreads,
-    out,
-  ];
-  console.log(args);
-  return magick({ args });
-}
-
-async function createFiller(out: Filename, dimensions: Dimensions) {
-  // const geomtry = await ()
-  const args = [
-    'convert',
-    '-size', `${dimensions.width}x${dimensions.height}`,
-    'xc:transparent',
-    out,
-  ];
-  return magick({ args });
-}
-
-async function renderSpreads(layout: Layout, spreads: Filename[]) {
-  const { gridLayout: { x, y } } = layout;
-  const capacity = x * y; 
-  const pages = [];
-  
-  let front = [];
-  let back = [];
-
-  for (let i = 0; i < spreads.length; i += 2) {
-    // adding images to the front is easy; they just go in order
-    const frontIdx = front.length;
-    front.push(spreads[i]);
-
-    // adding images to the back is harder, as we have to place right to left, top to bottom
-    const row = Math.trunc(frontIdx / x);
-    const column = frontIdx % x;
-    const reverseIdx = (row * x) + (x - column - 1);
-    console.log({ row, column, frontIdx, backIdx: reverseIdx});
-    back[reverseIdx] = spreads[i+1]
-
-    if (front.length === capacity) {
-      const frontFilename = `${tempDir}/result-${pages.length}-front-${pages.length / 2}.png`;
-      await renderOnePage(layout, front, frontFilename);
-      pages.push(frontFilename);
-      front = [];
-
-      const backFilename = `${tempDir}/result-${pages.length}-back-${Math.trunc(pages.length / 2)}.png`;
-      await renderOnePage(layout, back, backFilename);
-      pages.push(backFilename);
-      back = []
-    }
-  }
-
-  if (front.length > 0) {
-    const fillerDimensions = await getDimensions(spreads[0]);
-    const fillerImage = `${tempDir}/filler.png`;
-    await createFiller(fillerImage, fillerDimensions);
-
-    for (let i = 0; i < back.length; i++) {
-      if (!back[i]) {
-        back[i]  = fillerImage;
-      }
-    }
-
-    console.log(front, back);
-    
-    const frontFilename = `${tempDir}/result-${pages.length}-front-${pages.length / 2}.png`;
-    await renderOnePage(layout, front, frontFilename);
-    pages.push(frontFilename);
-    const backFilename = `${tempDir}/result-${pages.length}-back-${Math.trunc(pages.length / 2)}.png`;
-    await renderOnePage(layout, back, backFilename);
-    pages.push(backFilename);
-  }
-
-  return pages;
-}
